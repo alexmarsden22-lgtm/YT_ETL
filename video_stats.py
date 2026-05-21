ddb2e402cb8194c93f8773824cb7841b79a5cb9f
Alex Marsden <alexmarsden22@gmail.com>
2026-05-21 15:19:46 +0100

    Updated API to loop for video Ids

--- video_stats.py
+++ video_stats.py
@@ -6,6 +6,8 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv('API_KEY')
 CHANNEL_HANDLE = 'MrBeast'
+MAX_ITEMS = 50
+
 
 def get_playlist_id():
     try:
@@ -19,5 +21,38 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+
+def get_video_ids(playlist_id):
+    try:
+        video_list = []
+
+        page_token = None
+
+        base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={MAX_ITEMS}&playlistId={playlist_id}&key={API_KEY}'
+
+        while True:
+            url = base_url
+
+            if page_token:
+                url += f'&pageToken={page_token}'
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for i in data.get("items",[]):
+                video_list.append(i["contentDetails"]['videoId'])
+
+
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+
+        return video_list
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlist_id = get_playlist_id()
+    get_video_ids(playlist_id)
