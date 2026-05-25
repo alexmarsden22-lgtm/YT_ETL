03b10ac298ab280141ed37d2fedd1938cb8c4b08
Alex Marsden <alexmarsden22@gmail.com>
2026-05-25 20:24:20 +0100

    First DAG

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,16 +1,19 @@
 import requests
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from datetime import date
 import json
 
-load_dotenv(dotenv_path="./.env")
+from airflow.sdk import task
+from airflow.sdk import Variable
+ 
+#load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv('API_KEY')
-CHANNEL_HANDLE = 'MrBeast'
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 MAX_ITEMS = 50
 
-
+@task
 def get_playlist_id():
     try:
         url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
@@ -23,7 +26,7 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
-
+@task
 def get_video_ids(playlist_id):
     try:
         video_list = []
@@ -52,7 +55,7 @@ def get_video_ids(playlist_id):
         raise e
 
 
-
+@task
 def get_video_statistics(video_ids):
 
     def batch_list(video_ids, batch_size):
@@ -96,6 +99,7 @@ def get_video_statistics(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def save_to_json(extraction):
     file_path = f"data/YT_data_{date.today()}.json"
 
